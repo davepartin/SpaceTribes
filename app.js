b9e8eb8dc54ef0bcc325fbc1d0db25d2697185a8
davepartin <davepartin@gmail.com>
2025-07-26 19:24:41 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cron = require('node-cron');
-const path = require('path'); 
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname))); // <-- update here
 
 
 /*
